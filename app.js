289023bc49542531f4dd6b09225884fbd6136067
kaitou1412kid <kritagyashakya0@gmail.com>
2023-01-28 20:27:23 +0545

    port change

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.post("/",function(req, res){
     });  
 });
 
-app.listen(3000, function(){
-    console.log("Server running in port 3000");
+app.listen(3000 || process.env.PORT, function(){
+    console.log("Server running");
 });
